84f8fcd833f6f81e1fe1317e7d7a939c90d8dd44
Tyreik Rogers <tyreikr11@gmail.com>
2026-06-11 20:14:01 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        s_hashmap = {}
+        t_hashmap = {}
+
+        for s_letter in s:
+            if s_letter in s_hashmap:
+                s_hashmap[s_letter] += 1
+            else:
+                s_hashmap[s_letter] = 1
+
+        for t_letter in t:
+            if t_letter in t_hashmap:
+                t_hashmap[t_letter] += 1
+            else:
+                t_hashmap[t_letter] = 1
+
+        return s_hashmap == t_hashmap
